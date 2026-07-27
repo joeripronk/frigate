5287a4cd4508320e6911185128c09f8c63d53f64
joeripronk <git@joeri.nu>
2026-07-27 15:19:58 +0200

    fix ort

--- frigate/detectors/detection_runners.py
+++ frigate/detectors/detection_runners.py
@@ -10,6 +10,7 @@ from typing import Any
 import numpy as np
 import onnxruntime as ort
 from frigate.util.rknn_converter import is_rknn_compatible
+from frigate.util.model import get_ort_providers
 
 from frigate.detectors.detection_runners_cython import (
     nchw_to_nhwc_transpose,
